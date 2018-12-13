9acb7a4302a05dfd83ec0d69b8c510fc535c2c4b
jenn210 <jennifer.c.tuten@gmail.com>
2018-12-12 22:17:04 -0600

    add handlebars

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
+//Dependencies
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
@@ -16,29 +13,29 @@ var PORT = process.env.PORT || 3000;
 // Initialize Express
 var app = express();
 
+//Set up Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Configure mongoose
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytimesnba";
 mongoose.connect(MONGODB_URI);
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
+
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Make public a static folder
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/nytimesnba", { useNewUrlParser: true });
 
-// Main route (simple Hello World Message)
-app.get("/", function (req, res) {
-  res.send("Hello world");
-});
-
-app.get("/saved", function (req, res) {
-  res.send("Hello world");
-});
 
 // Scrape data from one site and place it into the mongodb db
 app.get("/scrape", function (req, res) {
